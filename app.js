a99b74b097cf23d5eeecc08315ecc8ddb43c2dda
asaduddin <asaduddin.z@samsung.com>
2018-01-18 11:00:34 +0700

    create campgrounds,  new, and header

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-// let grab us body of the form
+// let grab us body from the form method POST
 app.use(bodyParser.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
 
 let campgrounds = [
+    {name: 'Green Hill', image: 'https://farm5.staticflickr.com/4079/4805487492_618e66b63b.jpg'},
+    {name: 'Sapudi Island', image: 'https://farm9.staticflickr.com/8311/8021906474_d68de19953.jpg'},
+    {name: 'Raas Island', image: 'https://farm5.staticflickr.com/4134/4901707346_ec6b7d676a.jpg'},
+    {name: 'Green Hill', image: 'https://farm5.staticflickr.com/4079/4805487492_618e66b63b.jpg'},
+    {name: 'Sapudi Island', image: 'https://farm9.staticflickr.com/8311/8021906474_d68de19953.jpg'},
+    {name: 'Raas Island', image: 'https://farm5.staticflickr.com/4134/4901707346_ec6b7d676a.jpg'},
     {name: 'Green Hill', image: 'https://farm5.staticflickr.com/4079/4805487492_618e66b63b.jpg'},
     {name: 'Sapudi Island', image: 'https://farm9.staticflickr.com/8311/8021906474_d68de19953.jpg'},
     {name: 'Raas Island', image: 'https://farm5.staticflickr.com/4134/4901707346_ec6b7d676a.jpg'}
